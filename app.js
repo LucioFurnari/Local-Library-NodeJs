bdeb63a3b561eea34cb0b7094420e142fcfb91e0
LucioFurnari <luciofurnari99@gmail.com>
2023-09-15 20:51:24 -0300

    Add mongoose connection

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const mongoDb = 'mongodb+srv://harrow99:archwave99@cluster0.nrwzudw.mongodb.net/local_library?retryWrites=true&w=majority';
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDb);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
